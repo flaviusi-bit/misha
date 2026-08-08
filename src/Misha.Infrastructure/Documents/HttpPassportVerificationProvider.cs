119be30e0c7d59f51c70addcd9e9d16fa12ff589
flaviusi-bit <flaviusi@gmail.com>
2026-08-08 21:17:06 +0300

    fix: add missing configuration namespace

--- src/Misha.Infrastructure/Documents/HttpPassportVerificationProvider.cs
+++ src/Misha.Infrastructure/Documents/HttpPassportVerificationProvider.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
 using Misha.Application.Documents;
 using Misha.Domain.Documents;
 
