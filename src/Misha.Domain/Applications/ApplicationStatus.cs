02b532c41ff87965a3f84308416ee939bb10900e
flaviusi-bit <flaviusi@gmail.com>
2026-08-08 17:48:59 +0300

    feat: add application status model

---
+++ src/Misha.Domain/Applications/ApplicationStatus.cs
@@ -0,0 +1,11 @@
+namespace Misha.Domain.Applications;
+
+public enum ApplicationStatus
+{
+    Draft = 0,
+    Submitted = 1,
+    Processing = 2,
+    Approved = 3,
+    Refused = 4,
+    Cancelled = 5
+}
