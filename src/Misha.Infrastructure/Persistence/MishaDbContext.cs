dd877d75cd8e2f221d46e5a512090c9b82acd335
flaviusi-bit <flaviusi@gmail.com>
2026-08-08 19:04:15 +0300

    feat: map passport and watchlist persistence

--- src/Misha.Infrastructure/Persistence/MishaDbContext.cs
+++ src/Misha.Infrastructure/Persistence/MishaDbContext.cs
@@ -1,16 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using MishaApplication = Misha.Domain.Applications.Application;
+using Misha.Domain.Documents;
+using Misha.Domain.Watchlists;
 
 namespace Misha.Infrastructure.Persistence;
 
 public sealed class MishaDbContext(DbContextOptions<MishaDbContext> options) : DbContext(options)
 {
     public DbSet<MishaApplication> Applications => Set<MishaApplication>();
+    public DbSet<PassportDocument> PassportDocuments => Set<PassportDocument>();
+    public DbSet<WatchlistCheck> WatchlistChecks => Set<WatchlistCheck>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         var application = modelBuilder.Entity<MishaApplication>();
-
         application.ToTable("applications");
         application.HasKey(x => x.Id);
         application.Property(x => x.ApplicantReference).HasMaxLength(200).IsRequired();
@@ -19,5 +22,25 @@ public sealed class MishaDbContext(DbContextOptions<MishaDbContext> options) : D
         application.Property(x => x.RefusalReason).HasMaxLength(1000);
         application.HasIndex(x => x.ApplicantReference);
         application.HasIndex(x => x.Status);
+
+        var passport = modelBuilder.Entity<PassportDocument>();
+        passport.ToTable("passport_documents");
+        passport.HasKey(x => x.Id);
+        passport.Property(x => x.DocumentNumber).HasMaxLength(50).IsRequired();
+        passport.Property(x => x.IssuingCountry).HasMaxLength(3).IsRequired();
+        passport.Property(x => x.Surname).HasMaxLength(200).IsRequired();
+        passport.Property(x => x.GivenNames).HasMaxLength(200).IsRequired();
+        passport.Property(x => x.Nationality).HasMaxLength(3).IsRequired();
+        passport.HasIndex(x => x.ApplicationId).IsUnique();
+        passport.HasIndex(x => x.DocumentNumber);
+
+        var watchlist = modelBuilder.Entity<WatchlistCheck>();
+        watchlist.ToTable("watchlist_checks");
+        watchlist.HasKey(x => x.Id);
+        watchlist.Property(x => x.Provider).HasMaxLength(100).IsRequired();
+        watchlist.Property(x => x.Decision).HasConversion<string>().HasMaxLength(32).IsRequired();
+        watchlist.Property(x => x.MatchReference).HasMaxLength(200);
+        watchlist.Property(x => x.ErrorMessage).HasMaxLength(1000);
+        watchlist.HasIndex(x => new { x.ApplicationId, x.CreatedAtUtc });
     }
 }
