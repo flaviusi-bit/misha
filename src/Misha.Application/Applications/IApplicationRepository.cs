2c9a669e5965c80d82091d051187ee3fdca96144
flaviusi-bit <flaviusi@gmail.com>
2026-08-08 18:05:31 +0300

    fix: disambiguate Application domain type

--- src/Misha.Application/Applications/IApplicationRepository.cs
+++ src/Misha.Application/Applications/IApplicationRepository.cs
@@ -4,7 +4,7 @@ namespace Misha.Application.Applications;
 
 public interface IApplicationRepository
 {
-    Task<Application?> GetAsync(Guid id, CancellationToken cancellationToken);
-    Task AddAsync(Application application, CancellationToken cancellationToken);
+    Task<Misha.Domain.Applications.Application?> GetAsync(Guid id, CancellationToken cancellationToken);
+    Task AddAsync(Misha.Domain.Applications.Application application, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
