9777b56220c9cf9adc1a00643bbe4af47a1a2781
flaviusi-bit <flaviusi@gmail.com>
2026-08-08 17:50:27 +0300

    test: add application domain tests

---
+++ tests/Misha.Domain.Tests/ApplicationTests.cs
@@ -0,0 +1,35 @@
+using Misha.Domain.Applications;
+
+namespace Misha.Domain.Tests;
+
+public sealed class ApplicationTests
+{
+    [Fact]
+    public void Create_starts_as_draft()
+    {
+        var application = Application.Create("traveller-001");
+
+        Assert.Equal(ApplicationStatus.Draft, application.Status);
+        Assert.Equal("traveller-001", application.ApplicantReference);
+    }
+
+    [Fact]
+    public void Submit_moves_draft_to_submitted()
+    {
+        var application = Application.Create("traveller-001");
+
+        application.Submit();
+
+        Assert.Equal(ApplicationStatus.Submitted, application.Status);
+        Assert.NotNull(application.SubmittedAtUtc);
+    }
+
+    [Fact]
+    public void Submit_cannot_be_called_twice()
+    {
+        var application = Application.Create("traveller-001");
+        application.Submit();
+
+        Assert.Throws<InvalidOperationException>(() => application.Submit());
+    }
+}
