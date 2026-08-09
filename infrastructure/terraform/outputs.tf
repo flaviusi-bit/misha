77f6f352e3e88bfcaa308c8882a91efd211ce459
flaviusi-bit <flaviusi@gmail.com>
2026-08-09 10:20:39 +0300

    feat: expose AWS infrastructure outputs

---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "private_subnet_ids" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
+
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "documents_bucket_name" {
+  value = aws_s3_bucket.documents.bucket
+}
+
+output "application_events_queue_url" {
+  value = aws_sqs_queue.application_events.url
+}
+
+output "application_events_dlq_url" {
+  value = aws_sqs_queue.application_events_dlq.url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_api_task_definition" {
+  value = aws_ecs_task_definition.api.family
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "application_secret_arn" {
+  value = aws_secretsmanager_secret.application.arn
+}
