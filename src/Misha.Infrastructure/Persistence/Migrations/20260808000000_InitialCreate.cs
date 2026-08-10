f306780739009a5775553ebd32e3f1eb1c96a8e8
flaviusi-bit <flaviusi@gmail.com>
2026-08-10 13:41:14 +0300

    Fix duplicate EF Core DbContext metadata

--- src/Misha.Infrastructure/Persistence/Migrations/20260808000000_InitialCreate.cs
+++ src/Misha.Infrastructure/Persistence/Migrations/20260808000000_InitialCreate.cs
@@ -1,13 +1,10 @@
 using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Misha.Infrastructure.Persistence.Migrations;
 
-[DbContext(typeof(MishaDbContext))]
 [Migration("20260808000000_InitialCreate")]
 public partial class InitialCreate : Migration
 {
