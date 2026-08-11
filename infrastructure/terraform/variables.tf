b9d5f1edb621cee200acc24fe79edd7b5517f246
flaviusi-bit <flaviusi@gmail.com>
2026-08-11 03:35:24 +0300

    feat(auth): configure Cognito callback settings

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -76,3 +76,15 @@ variable "enable_deletion_protection" {
   description = "Protect production databases from accidental deletion."
   default     = false
 }
+
+variable "cognito_callback_urls" {
+  type        = list(string)
+  description = "OAuth2 authorization-code callback URLs for the MISHA web client."
+  default     = ["http://localhost:3000/callback"]
+}
+
+variable "cognito_logout_urls" {
+  type        = list(string)
+  description = "OAuth2 logout callback URLs for the MISHA web client."
+  default     = ["http://localhost:3000/"]
+}
