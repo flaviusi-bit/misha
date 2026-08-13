c747bc8dfc116736b945e4ea42dd1409c893105c
flaviusi-bit <flaviusi@gmail.com>
2026-08-13 12:34:01 +0300

    fix(terraform): canonicalize IAM policy formatting

--- infrastructure/terraform/oidc.tf
+++ infrastructure/terraform/oidc.tf
@@ -133,7 +133,7 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
         "s3:GetObject",
         "s3:PutObject",
         "s3:DeleteObject"
-        ], Resource = [
+      ], Resource = [
         "arn:aws:s3:::misha-terraform-state/misha/dev/terraform.tfstate",
         "arn:aws:s3:::misha-terraform-state/misha/dev/terraform.tfstate.tflock"
       ] },
@@ -159,7 +159,7 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
         "sqs:SetQueueAttributes",
         "sqs:TagQueue",
         "sqs:UntagQueue"
-        ], Resource = [
+      ], Resource = [
         "arn:aws:sqs:eu-central-1:576984879588:misha-dev-application-events",
         "arn:aws:sqs:eu-central-1:576984879588:misha-dev-application-events-dlq"
       ] },
@@ -231,7 +231,7 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
         "backup:GetBackupSelection",
         "backup:ListBackupSelections",
         "backup:ListTags"
-        ], Resource = "*" },
+      ], Resource = "*" },
       { Effect = "Allow", Action = [
         "iam:GetRole",
         "iam:CreateRole",
@@ -246,7 +246,7 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
         "iam:ListAttachedRolePolicies",
         "iam:TagRole",
         "iam:UntagRole"
-        ], Resource = [
+      ], Resource = [
         aws_iam_role.github_actions_deploy.arn,
         aws_iam_role.ecs_execution.arn,
         aws_iam_role.ecs_task.arn,
