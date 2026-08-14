b678e6337937a1f7bfaaef0cc7ea99fd473becf0
flaviusi-bit <flaviusi@gmail.com>
2026-08-15 01:08:37 +0300

    feat(worker): grant deployment pipeline worker ECR access

---
+++ infrastructure/terraform/worker-deploy-permissions.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role_policy" "github_actions_worker_deploy" {
+  name = "${local.name}-github-actions-worker-deploy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:DeleteLifecyclePolicy",
+          "ecr:DescribeRepositories",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:ListTagsForResource",
+          "ecr:PutImage",
+          "ecr:PutImageScanningConfiguration",
+          "ecr:PutImageTagMutability",
+          "ecr:PutLifecyclePolicy",
+          "ecr:TagResource",
+          "ecr:UntagResource",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = aws_ecr_repository.worker.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:ListTagsForResource",
+          "logs:CreateLogGroup",
+          "logs:DeleteLogGroup",
+          "logs:PutRetentionPolicy",
+          "logs:DeleteRetentionPolicy",
+          "logs:TagResource",
+          "logs:UntagResource"
+        ]
+        Resource = "${aws_cloudwatch_log_group.worker.arn}:*"
+      }
+    ]
+  })
+}
