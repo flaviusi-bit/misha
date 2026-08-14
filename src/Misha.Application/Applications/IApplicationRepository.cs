0ae5949675b5ed1562caf503c2c0c176a0e12b0c
flaviusi-bit <flaviusi@gmail.com>
2026-08-14 10:55:10 +0300

    feat: support idempotent application lookup

--- src/Misha.Application/Applications/IApplicationRepository.cs
+++ src/Misha.Application/Applications/IApplicationRepository.cs
@@ -5,6 +5,7 @@ namespace Misha.Application.Applications;
 public interface IApplicationRepository
 {
     Task<Misha.Domain.Applications.Application?> GetAsync(Guid id, CancellationToken cancellationToken);
+    Task<Misha.Domain.Applications.Application?> GetByIdempotencyKeyAsync(string idempotencyKey, CancellationToken cancellationToken);
     Task AddAsync(Misha.Domain.Applications.Application application, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
