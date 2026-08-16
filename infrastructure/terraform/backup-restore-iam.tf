73473e2f24605d812d23e6494a2d7daef50eaab6
flaviusi-bit <flaviusi@gmail.com>
2026-08-16 03:21:13 +0300

    fix(terraform): format restore testing IAM policy

--- infrastructure/terraform/backup-restore-iam.tf
+++ infrastructure/terraform/backup-restore-iam.tf
@@ -6,8 +6,8 @@ resource "aws_iam_role_policy" "github_actions_backup_restore_testing" {
     Version   = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect    = "Allow"
+        Action    = [
           "backup:CreateRestoreTestingPlan",
           "backup:DeleteRestoreTestingPlan",
           "backup:GetRestoreTestingPlan",
