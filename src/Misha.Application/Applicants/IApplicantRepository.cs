818b02ce2622b1a4161373e079b8415f248388ec
flaviusi-bit <flaviusi@gmail.com>
2026-08-24 13:34:19 +0300

    Remove unused applicant repository contract

--- src/Misha.Application/Applicants/IApplicantRepository.cs
+++
@@ -1,9 +0,0 @@
-using Misha.Domain.Applicants;
-
-namespace Misha.Application.Applicants;
-
-public interface IApplicantRepository
-{
-    Task<Applicant?> GetAsync(Guid id, CancellationToken cancellationToken);
-    Task SaveChangesAsync(CancellationToken cancellationToken);
-}
