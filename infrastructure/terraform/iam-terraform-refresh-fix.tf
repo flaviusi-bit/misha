20f76e1dea628c191f1329c5b33409e1f3a1374b
flaviusi-bit <flaviusi@gmail.com>
2026-08-25 13:39:06 +0300

    fix IAM policy tag permission bootstrap

--- infrastructure/terraform/iam-terraform-refresh-fix.tf
+++ infrastructure/terraform/iam-terraform-refresh-fix.tf
@@ -1,20 +1,33 @@
 # IAM ListInstanceProfilesForRole is a List action and must be granted on
 # the wildcard resource. Terraform needs it while reading/deleting IAM roles.
-# iam:CreatePolicy is temporarily required because the backup-restore recovery
-# refactor creates a managed policy before it can attach it to the deploy role.
+# iam:CreatePolicy is required because the backup-restore recovery refactor
+# creates a managed policy before it can attach it to the deploy role.
+# iam:TagPolicy and iam:ListPolicyTags are required because the AWS provider
+# sends provider default_tags with the managed policy CreatePolicy request.
 resource "aws_iam_role_policy" "github_actions_iam_refresh" {
   name = "${local.name}-github-actions-iam-refresh"
   role = aws_iam_role.github_actions_deploy.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "iam:ListInstanceProfilesForRole",
-        "iam:CreatePolicy"
-      ]
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:ListInstanceProfilesForRole",
+          "iam:CreatePolicy"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:ListPolicyTags",
+          "iam:TagPolicy",
+          "iam:UntagPolicy"
+        ]
+        Resource = "arn:aws:iam::576984879588:policy/${local.name}-*"
+      }
+    ]
   })
 }
