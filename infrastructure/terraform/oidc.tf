f4c63a7d39837b6422450fb3914a5e0b25918863
flaviusi-bit <flaviusi@gmail.com>
2026-08-29 18:18:12 +0300

    fix(terraform): fold alerting permissions into deploy policy

--- infrastructure/terraform/oidc.tf
+++ infrastructure/terraform/oidc.tf
@@ -314,7 +314,28 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
         "cloudwatch:GetDashboard",
         "cloudwatch:PutDashboard",
         "cloudwatch:DeleteDashboard"
-      ], Resource = "*" }
+      ], Resource = "*" },
+      { Effect = "Allow", Action = [
+        "sns:CreateTopic",
+        "sns:DeleteTopic",
+        "sns:GetTopicAttributes",
+        "sns:SetTopicAttributes",
+        "sns:ListTagsForResource",
+        "sns:TagResource",
+        "sns:UntagResource",
+        "sns:Subscribe",
+        "sns:Unsubscribe",
+        "sns:ListSubscriptionsByTopic"
+      ], Resource = "arn:aws:sns:${var.aws_region}:576984879588:${local.name}-operational-alerts" },
+      { Effect = "Allow", Action = [
+        "events:PutRule",
+        "events:DeleteRule",
+        "events:DescribeRule",
+        "events:TagResource",
+        "events:UntagResource",
+        "events:PutTargets",
+        "events:RemoveTargets"
+      ], Resource = "arn:aws:events:${var.aws_region}:576984879588:rule/${local.name}-operational-alarm-state-change" }
     ]
   })
 }
