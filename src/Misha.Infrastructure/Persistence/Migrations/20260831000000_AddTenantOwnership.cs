d83a029fede96ab27050a459e50f133fafc5aa84
flaviusi-bit <flaviusi@gmail.com>
2026-08-31 12:19:45 +0300

    fix: import EF DbContext attribute namespace

--- src/Misha.Infrastructure/Persistence/Migrations/20260831000000_AddTenantOwnership.cs
+++ src/Misha.Infrastructure/Persistence/Migrations/20260831000000_AddTenantOwnership.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 #nullable disable
 namespace Misha.Infrastructure.Persistence.Migrations;
