f7438e3c97356d6358bbb8f94482fd2a08468456
flaviusi-bit <flaviusi@gmail.com>
2026-09-09 17:31:25 +0300

    revert accidental main-only security helper commit

--- src/Misha.Api/ApiErrorResponses.cs
+++
@@ -1,8 +0,0 @@
-namespace Misha.Api;
-
-internal static class ApiErrorResponses
-{
-    public const string NotFound = "The requested resource was not found.";
-    public const string InvalidRequest = "The request could not be processed.";
-    public const string InvalidOperation = "The requested operation could not be completed.";
-}
